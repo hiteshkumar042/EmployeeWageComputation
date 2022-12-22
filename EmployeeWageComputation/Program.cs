fd01e26e05b4b5fd6d745ae8779e604210e7d563
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-22 14:08:40 +0530

    Calculate for 20 Working days

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,33 +8,33 @@
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0;
-            int empWage = 0;
+            const int MAX_WORKING_DAYS = 20;
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1;
 
             //UC-1 Check employee present or not
             Random random = new Random();
-            int employeeInput = random.Next(0, 3);
-            //UC4 using Switch case
-            switch (employeeInput) 
+            //UC-5 Wage for 20 Working days
+            for (day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("FullTime Employee is Present");
-                    empHrs = 8;
-                    break;
-                case PART_TIME:
-
-                    Console.WriteLine("PartTime Employee is Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                int employeeInput = random.Next(0, 3);
+                //UC-4- Switch case
+                switch (employeeInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                //UC-2 & UC-3 calculate Employee Wage
+                empWage = EMP_RATE_PER_HR * empHrs;
+                totalWage += empWage;
             }
-
-            //UC-2 & UC-3 calculate Employee Wage
-            empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Employee wage is :" + empWage);
+            Console.WriteLine("Total wage for {0} working days:{1}", (day - 1), totalWage);
         }
     }
 }

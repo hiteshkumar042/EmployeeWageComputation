82dd429fd06bbda1a4e34e6baa642d8dffe24b5a
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-22 13:54:45 +0530

    Daily Employee Wage Added

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,16 +7,24 @@
             Console.WriteLine("Welcome to Employee Wage Computation!");
             //Emp Attandance - UC1
             const int EMP_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int randomInput = random.Next(0, 2);
             if (randomInput == EMP_PRESENT)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empWage = 0;
             }
+            //UC-2 calculate Employee Wage
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage is :" + empWage);
         }
     }
 }

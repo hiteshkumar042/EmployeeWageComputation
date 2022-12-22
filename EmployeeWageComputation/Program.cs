832b4b2c846dc22d17d074e82d6e61f80a513ce5
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-22 13:51:59 +0530

    Modified for Employee Attendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation!");
+            //Emp Attandance - UC1
+            const int EMP_PRESENT = 1;
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (randomInput == EMP_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }

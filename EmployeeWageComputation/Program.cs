5b6a709a8daad3a633e70c871f8a2cf5e344bce8
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-22 14:14:29 +0530

    Wage for 20 WorkingDay or 100 WorkingHrs

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -9,12 +9,13 @@
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
-            int empHrs = 0, empWage = 0, totalWage = 0, day = 1;
+            const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
 
             //UC-1 Check employee present or not
             Random random = new Random();
-            //UC-5 Wage for 20 Working days
-            for (day = 1; day <= MAX_WORKING_DAYS; day++)
+            //UC-6 Wage for 20 Working days or 100 Hrs
+            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int employeeInput = random.Next(0, 3);
                 //UC-4- Switch case
@@ -30,11 +31,13 @@
                         empHrs = 0;
                         break;
                 }
-                //UC-2 & UC-3 calculate Employee Wage
+                //UC-2 calculate EmployeeWage
                 empWage = EMP_RATE_PER_HR * empHrs;
                 totalWage += empWage;
+                totalHrs += empHrs;
+                day++;
             }
-            Console.WriteLine("Total wage for {0} working days:{1}", (day - 1), totalWage);
+            Console.WriteLine("Total wage for {0} days:{1} and Hrs:{2}", (day - 1), totalWage, (totalHrs - empHrs));
         }
     }
 }
